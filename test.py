00767bd5eb54aa65cf1612067f912ba41c606fca
Gabrijel CIGULA <gabrijel.cigula@knapp.com>
2023-09-27 13:22:35 +0200

    Dodavanje nove naredbe

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 
+#Test
 a = int(input("Unesi prvi broj:"))
 b = int(input("Unesi drugi broj:"))
 c=a+b
 d=a*b
 e=a/b
+g=a+b+c+d+e
 print("Zbroj je =", c)
 print("Umnožak je=", d)
 print("a/b je=", e)
+print("Ukupan rezultat iznosi=", g)
