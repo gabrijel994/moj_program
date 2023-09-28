29240776b0b948ae434fd8874f4e0027c4f4d3b2
Gabrijel CIGULA <gabrijel.cigula@knapp.com>
2023-09-28 12:11:10 +0200

    Dodavanje novih programa

---
+++ proba.py
@@ -0,0 +1,35 @@
+#a=int(input("Unesi cijeli broj:"))
+#      else("Uneseni broj nije cijeli broj!"))
+#print(a)
+#
+#b=float(input("Unesi realan broj:"))
+#print(b)
+#c=input("Unesi ime:")
+#print(c)
+
+# d=input("Unesi prvu riječ: ")
+# e=(d+" ")*5
+# print(e)
+
+# f=input("Unesite drugu riječ: ")
+# g= d +' '+ f
+# print(g)
+
+#Unjeti troznamenkasti broj, a kao ispis mora se prikazati taj broj + vrijednost u decimalnom obliku
+
+# Troznamenkasti_broj=int(input("Unesite troznamenkasti broj:"))
+# binarni_broj=bin(Troznamenkasti_broj)
+
+# print("Uneseni troznamenkasti broj iznosi: ", , "a binarna vrijednost broja je: ", binarni_broj)
+
+# Unesite_znak=input("Unesite znak: ")
+# znak=ord(Unesite_znak)
+# print("Vas znak",Unesite_znak,"ASCII kod",znak)
+
+a=int(input("Enetr the number:"))
+if a > 5:
+    print("Your number is bigger then 5")
+if a < 5:
+    print("Your number is smaller then 5")
+else:
+    print("Your number is 5")
